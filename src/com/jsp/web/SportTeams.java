ea7ba027a5a31fae1f6bd7b32727cdd60dfec501
mannidev <kjoedev@gmail.com>
2019-12-13 22:54:43 -0500

    style results page

--- src/com/jsp/web/SportTeams.java
+++ src/com/jsp/web/SportTeams.java
@@ -12,37 +12,36 @@ import javax.servlet.http.HttpServletResponse;
 import com.jsp.model.SportTeamRecomender;
 import com.jsp.model.Team;
 
-public class SportTeams extends HttpServlet 
-{ 
+public class SportTeams extends HttpServlet
+{
     private static final long serialVersionUID = 1L;
 
     @Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
-			throws ServletException, IOException 
-	{
-		try
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        try
         {
-		    String sport = req.getParameter("sport");
-	        SportTeamRecomender recomender = new SportTeamRecomender();
-	        List<Team> teams = recomender.getTeams(sport, "usa");
-	        
-	        resp.setContentType("text/html");
-	        
-	        PrintWriter out = resp.getWriter();
-	        out.println("<div>");
-	        out.println("<b>Available Teams</b>");
-	        out.println("</div>");
-	        out.println("<div>");
-	        out.println("<ul>");
-	        for(Team team : teams)
-	        {
-	            out.println("<li>" + team.getName() + "</li>");
-	        }
-	        out.println("</ul>");
-	        out.println("</div>");
+            String sport = req.getParameter("sport");
+            SportTeamRecomender recomender = new SportTeamRecomender();
+            List<Team> teams = recomender.getTeams(sport, "usa");
+
+            resp.setContentType("text/html");
+
+            PrintWriter out = resp.getWriter();
+            out.println("<div class=\"header\">");
+            out.println("<h1>" + sport + " Teams</h1>");
+            out.println("</div>");
+            out.println("<div class=\"body\">");
+            out.println("<ul>");
+            for (Team team : teams)
+            {
+                out.println("<li>" + team.getName() + "</li>");
+            }
+            out.println("</ul>");
+            out.println("</div>");
         } catch (Exception e)
         {
             e.printStackTrace();
-        }		        
-	}
+        }
+    }
 }
