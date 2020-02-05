7d3e6bd9b560e2f74a24a12f2a9d5652fce854ff
mannidev <kjoedev@gmail.com>
2020-02-05 08:49:36 -0500

    pint hello servlet on browser from a servlet

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # JAVA 
-A simple app that prints hello jsp from a servlet using the response objects `resp.getWriter()`
+A simple app that prints hello Servlet from a servlet using the response objects `resp.getWriter()`
+
+We also set the content type of the response using `resp.setContentType("text/html;charset=UTF-8");`
+
+This must be called before the response is committed of `resp.getWriter()` is called. 
+
+Doing this tells the client what content to expect and helps the client handle the content.
--- src/com/jsp/web/HelloServlet.java
+++ src/com/jsp/web/HelloServlet.java
@@ -8,15 +8,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloJsp extends HttpServlet
+public class HelloServlet extends HttpServlet
 { 
     private static final long serialVersionUID = 1L;
     
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
+        resp.setContentType("text/html;charset=UTF-8");
         PrintWriter writer = resp.getWriter();
-        writer.println("Hello Jsp");
+        writer.println("<h1>Hello Servlet</h1>");
     }
 
 }
